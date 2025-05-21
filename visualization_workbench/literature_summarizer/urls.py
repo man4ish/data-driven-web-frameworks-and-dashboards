6d70961dcaea546ab30414d743a87aa2754983e5
mkumar4 <mkumar4@tenor.ifx.kumc.edu>
2025-05-21 11:14:37 -0500

    Update home/home.html

--- visualization_workbench/literature_summarizer/urls.py
+++ visualization_workbench/literature_summarizer/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'literature_summarizer'  # <-- THIS IS ESSENTIAL
+
 urlpatterns = [
     path("", views.summarize_view, name="literature_summary"), 
     path("trends/", views.trend_analysis, name="trend_analysis"),
